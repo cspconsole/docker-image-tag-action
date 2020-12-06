aac29c9635d9768eefc2f42e1ddeba33a620a7ad
Martin Cambal <martin@cspconsole.com>
2020-12-06 14:13:14 +0100

    Removed unsupported if

--- git-push.sh
+++ git-push.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 set -e
 
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
+if [[ "$IS_NEW_TAG" == 'true' ]];
+then
+  git config --local user.email "action@github.com"
+  git config --local user.name "GitHub Action"
 
-git tag -a $TAG -m "Release $TAG"
+  git tag -a $TAG -m "Release $TAG"
 
-echo "Pushing to branch $BRANCH";
-[ -z "${GITHUB_TOKEN}" ] && {
-    echo 'Missing input "github-token"';
-    exit 1;
-};
+  echo "Pushing to branch $BRANCH";
+  [ -z "${GITHUB_TOKEN}" ] && {
+      echo 'Missing input "github-token"';
+      exit 1;
+  };
 
-REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git push "$REMOTE_REPO" HEAD:$BRANCH --follow-tags --tags;
+  git push "$REMOTE_REPO" HEAD:$BRANCH --follow-tags --tags;
+else
+  echo "Doing nothing. Tag $TAG exists"
+fi
