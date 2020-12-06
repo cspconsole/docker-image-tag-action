b8f6db374b9f0dd3bd86da0eeddbf188cac74e69
Martin Cambal <martin@cspconsole.com>
2020-12-06 15:07:05 +0100

    Added debuggin lines

--- git-push.sh
+++ git-push.sh
@@ -7,15 +7,19 @@ then
 
   git tag -a $TAG -m "Release $TAG"
 
-  echo "Pushing to branch $BRANCH";
-  [ -z "${GITHUB_TOKEN}" ] && {
-      echo 'Missing input "github-token"';
-      exit 1;
-  };
+  echo "Pushing to branch $BRANCH"
+  echo "Tag : $TAG"
+  echo "Token $GITHUB_TOKEN"
+
+  if [[ -z "$GITHUB_TOKEN" ]]
+  then
+      echo 'Missing input "github-token"'
+      exit 1
+  fi
 
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-  git push "$REMOTE_REPO" HEAD:$BRANCH --follow-tags --tags;
+  git push "$REMOTE_REPO" HEAD:$BRANCH --follow-tags --tags
 else
   echo "Doing nothing. Tag $TAG exists"
 fi
